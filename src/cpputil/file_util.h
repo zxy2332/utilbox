88e26f47178a8157386e3ec803da727120368629
zxy2332 <zxy2332@gmail.com>
2024-01-29 17:23:29 +0800

    添加C++17以前版本filesystem适配

--- src/cpputil/file_util.h
+++ src/cpputil/file_util.h
@@ -1,7 +1,18 @@
 #pragma once
 #include "cpputil_export.h"
 #include <string>
+#if _HAS_CXX17
+#include <filesystem>
+#else
+#include <experimental/filesystem>
+#endif
+
 namespace zxy::util {
+#if _HAS_CXX17
+namespace fs = std::filesystem;
+#else
+    namespace fs = std::experimental::filesystem;
+#endif
     //写文件
     CPPUTIL_EXPORT bool write_file(const std::string &file_name,const std::string &file_content);
     //读取文件内容
